b96926ef832c00f8ff67dc52bd7efaa05a118be3
Jonathan S. Katz <jonathan.katz@tufts.edu>
2009-01-26 16:00:21 -0500

    updated DynamicSifter to work with scopes that take lambda methods

--- lib/dynamic_sifter.rb
+++ lib/dynamic_sifter.rb
@@ -4,7 +4,13 @@ class DynamicSifter
   #
   # * <tt>model</tt> - an <tt>ActiveRecord</tt> class
   # * <tt>args</tt> - a hash of arguments.  These are the same parameters that you pass to <tt>ActiveRecord::Base#find</tt>, with one addition:
-  #   *<tt>:filters</tt> - used to specify the <tt>named_scopes</tt> to use to filter the search.  Can either pass a <tt>String</tt> for one filter, otherwise an <tt>Array</tt> of symbols.
+  #   *<tt>:filters</tt> - used to specify the <tt>named_scopes</tt> to use to filter the search.  Can pass either a:
+  #     *<tt>String</tt> for one filter
+  #     *<tt>Array</tt> of symbols for multiple symbols
+  #     *<tt>Hash</tt> of symbols of Arrays, where the Arrays contain arguments to the scope (e.g. { :author_and_publisher => [arg1, arg2] }) 
+  #
+  # Examples:
+  # DynamicSifter.search()
   def self.search(model, args={})
     self.new(model, args).search
   end
@@ -12,8 +18,7 @@ class DynamicSifter
   def initialize(model, args={})
     @model = model
     @scopes = model.scopes.map { |k,v| k.to_sym }
-    @filters = args.delete(:filters) || []
-    @filters = @filters.to_a if @filters.kind_of? String
+    @filters = initialize_filters(args.delete(:filters))
     @options = args
   end
 
@@ -24,7 +29,20 @@ class DynamicSifter
 private
 
   def apply_filters(results)
-    @filters.inject(results) { |result,filter| result.send(filter.to_sym) if filter_exists?(filter) }
+    @filters.inject(results) { |result,filter|
+      ( filter.respond_to?(:first) ?
+        result.send(filter.first.to_sym, *filter.last) : 
+        result.send(filter.to_sym) ) if filter_exists?(filter)
+    }
+  end
+
+  def initialize_filters(filters)
+    case filters.class
+    when String: filters.to_a
+    when Hash: filters.to_a
+    else
+      filters
+    end
   end
 
   def filter_exists?(filter)
