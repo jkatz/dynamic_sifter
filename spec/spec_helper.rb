153a06b78b8787ba1f7d9ce646a64809ccf04d43
Brian Mitchell <brian.mitchell@excoventures.com>
2009-02-10 20:04:37 -0500

    Add some basic tables from which we will build some models.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,30 @@
 require 'rubygems'
 require 'spec'
+require 'active_record'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+require 'dynamic_sifter'
 
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => ':memory:')
+
+ActiveRecord::Schema.define do
+
+  create_table :bottles do |bottle|
+    bottle.references :wine
+    bottle.integer :year, :base_price
+  end
+
+  create_table :wines do |wine|
+    wine.references :region
+    wine.string :name, :kind
+    wine.text :description
+  end
+
+  create_table :regions do |region|
+    region.string :name
+    region.text :description
+  end
+
+end
